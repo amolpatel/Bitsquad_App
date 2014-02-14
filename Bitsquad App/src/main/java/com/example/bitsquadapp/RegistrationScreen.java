1b479b121d851e20ae4a4a8e0623d74a9ba59cab
caillousweat <lt.washington9@gmail.com>
2014-02-14 18:51:11 -0500

    Registration screen working

--- Bitsquad App/src/main/java/com/example/bitsquadapp/RegistrationScreen.java
+++ Bitsquad App/src/main/java/com/example/bitsquadapp/RegistrationScreen.java
@@ -3,6 +3,8 @@ package com.example.bitsquadapp;
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -10,6 +12,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegistrationScreen extends Activity {
 
@@ -44,6 +48,42 @@ public class RegistrationScreen extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+
+    public void registerUser(View view){
+        EditText newUsername   = (EditText)findViewById(R.id.NewUsername);
+        EditText newName = (EditText)findViewById(R.id.NewName);
+        EditText newPassword = (EditText)findViewById(R.id.NewPassword);
+        EditText confirmPassword = (EditText)findViewById(R.id.ConfirmPassword);
+
+
+        Context context = this.getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
+        CharSequence text;
+
+        //
+        if(!newPassword.getText().toString().equals(confirmPassword.getText().toString())) {
+            text = "Re-type Password";
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }
+
+        if(newUsername == null || newName == null || newPassword == null){
+            text="Please fill out all fields";
+            Toast toast = Toast.makeText(context,text,duration);
+            toast.show();
+        }
+        else{
+            ((MyApplication) getApplication()).addUser(
+                    newName.getText().toString()
+                    , newPassword.getText().toString()
+                    , newUsername.getText().toString()
+            );
+
+            Intent loginScreenIntent = new Intent(this, LoginScreen.class);
+            startActivity(loginScreenIntent);
+        }
     }
 
     /**
